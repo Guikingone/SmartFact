8d68eb10680cfae4b8c0f99652696bdf28a4dac7
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-06-11 17:28:19 +0200

    [ADD](Docker)[!M] Docker|Progress on configuration

--- src/AppBundle/Controller/Web/DefaultController.php
+++ src/AppBundle/Controller/Web/DefaultController.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace AppBundle\Controller;
+/*
+ * This file is part of the SmartFact project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Controller\Web;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the SmartFact project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * Class User
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class User
+{
+
+}
