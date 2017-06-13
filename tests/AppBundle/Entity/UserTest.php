198805d0e98085a1b0b46f1b49a1fefbb203bdc7
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-06-13 10:41:08 +0200

    [FIX](Docker)[!M] Docker configuration|Progress on Env

---
+++ tests/AppBundle/Entity/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the SmartFact project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace tests\AppBundle\Entity;
+
+/**
+ * Class UserTest
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class UserTest
+{
+
+}
