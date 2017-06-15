3fc75ecbfe49a7417e8b529948c6304698437ae2
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-06-15 13:51:21 +0200

    [ADD](Tests)[!I1_Entity] Tests|User in progress

--- core/src/AppBundle/Entity/User.php
+++ core/src/AppBundle/Entity/User.php
@@ -18,5 +18,305 @@ namespace AppBundle\Entity;
  */
 class User
 {
+    /**
+     * @var string
+     */
+    private $firstname;
 
+    /**
+     * @var string
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * @var string
+     */
+    private $phoneNumber;
+
+    /**
+     * @var \DateTime
+     */
+    private $birthDate;
+
+    /**
+     * @var string
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var bool
+     */
+    private $validated;
+
+    /**
+     * @var bool
+     */
+    private $isActive;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var string
+     */
+    private $plainPassword;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @var array
+     */
+    private $roles;
+
+    /**
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname($firstName)
+    {
+        $this->firstname = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname($lastName)
+    {
+        $this->lastname = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+    /**
+     * @param \DateTime $birthDate
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsValidated()
+    {
+        return $this->validated;
+    }
+
+    /**
+     * @param bool $validated
+     */
+    public function setValidated($validated)
+    {
+        $this->validated = $validated;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param bool $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * @param string $roles
+     */
+    public function addRoles($roles)
+    {
+        $this->roles[] = $roles;
+    }
 }
--- core/tests/AppBundle/Entity/UserTest.php
+++ core/tests/AppBundle/Entity/UserTest.php
@@ -11,12 +11,47 @@
 
 namespace tests\AppBundle\Entity;
 
+use AppBundle\Entity\User;
+
 /**
  * Class UserTest
  *
  * @author Guillaume Loulier <contact@guillaumeloulier.fr>
  */
-class UserTest
+class UserTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Test the instantiation of the User entity.
+     */
+    public function testUserInstance()
+    {
+        $user = new User();
+        $user->setFirstName('Harry');
+        $user->setLastName('Potter');
+        $user->setAddress('23 Poudlard Avenue');
+        $user->setPhoneNumber('0635459287');
+        $user->setBirthDate(new \DateTime());
+        $user->setStatus('Freelance');
+        $user->setCreatedAt(new \DateTime());
+        $user->setValidated(true);
+        $user->setIsActive(true);
+        $user->setUsername('HP');
+        $user->setPassword('LB,L8ELTDL0');
+        $user->setToken('tok_0010901_001NNDOPPPANDHYEMMANDU');
+        $user->addRoles('ROLE_USER');
 
+        $this->assertEquals('Harry', $user->getFirstName());
+        $this->assertEquals('Potter', $user->getLastName());
+        $this->assertEquals('23 Poudlard Avenue', $user->getAddress());
+        $this->assertEquals('0635459287', $user->getPhoneNumber());
+        $this->assertEquals(new \DateTime(), $user->getBirthDate());
+        $this->assertEquals('Freelance', $user->getStatus());
+        $this->assertEquals(new \DateTime(), $user->getCreatedAt());
+        $this->assertTrue($user->getIsValidated());
+        $this->assertTrue($user->getIsActive());
+        $this->assertEquals('HP', $user->getUsername());
+        $this->assertEquals('LB,L8ELTDL0', $user->getPassword());
+        $this->assertEquals('tok_0010901_001NNDOPPPANDHYEMMANDU', $user->getToken());
+        $this->assertContains('ROLE_USER', $user->getRoles());
+    }
 }
