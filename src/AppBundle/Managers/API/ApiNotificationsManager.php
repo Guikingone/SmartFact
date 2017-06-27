3e7171585eca621f9f3b26a982bd2b3f42990fb0
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-06-27 19:45:22 +0200

    [ADD](Actions)[!I1_Entity] Actions|Progress + To test

--- src/AppBundle/Managers/API/ApiNotificationsManager.php
+++ src/AppBundle/Managers/API/ApiNotificationsManager.php
@@ -11,6 +11,12 @@
 
 namespace AppBundle\Managers\API;
 
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Serializer\Serializer;
+
+// Entity
+use AppBundle\Entity\Notifications;
+
 /**
  * Class ApiNotificationsManager
  *
@@ -18,4 +24,80 @@ namespace AppBundle\Managers\API;
  */
 class ApiNotificationsManager
 {
+    /** @var EntityManager */
+    private $doctrine;
+
+    /** @var Serializer */
+    private $serializer;
+
+    /**
+     * ApiNotificationsManager constructor.
+     *
+     * @param EntityManager $doctrine
+     * @param Serializer $serializer
+     */
+    public function __construct(
+        EntityManager $doctrine,
+        Serializer $serializer
+    ) {
+        $this->doctrine = $doctrine;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Return the User notifications.
+     *
+     * @param int $id   The id of the User.
+     *
+     * @return array|null   The array of Notifications if available, null if not.
+     */
+    public function getUserNotifications($id)
+    {
+        $data = $this->doctrine->getRepository(Notifications::class)
+                               ->findBy([
+                                   'user' => $id
+                               ]);
+
+        if (!$data) {
+            return null;
+        }
+
+        return $this->serializer->serialize(
+            $data,
+            'json',
+            ['groups' => ['notifications']]
+        );
+    }
+
+    /**
+     * Return a single Notification using both
+     * User id and Notification id.
+     *
+     * @param int $id               The User id.
+     * @param int $notificationId   The Notification id.
+     *
+     * @return array|null   The array of Notification if available, null if not.
+     */
+    public function getUserSingleNotification($id, $notificationId)
+    {
+        $data = $this->doctrine->getRepository(Notifications::class)
+                               ->findBy([
+                                   'user' => $id,
+                                   'id' => $notificationId
+                               ]);
+
+        if (!$data) {
+            return null;
+        }
+
+        return $this->serializer->serialize(
+            $data,
+            'json'
+        );
+    }
+
+    public function postUserNotification($data)
+    {
+
+    }
 }
