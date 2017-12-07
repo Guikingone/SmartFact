8b8c3ffd2423a9f412dedf7fe0f77c6b0b7895bd
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-12-07 09:39:44 +0100

    feat(vue): vue|Added & Configured via Encore

---
+++ assets/vue/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Home',
+        data () {
+            return {
+                msg: 'Hello World !'
+            }
+        }
+    }
+</script>
